009cdbaf01d9537e4ede0531fcd2d61f7b26ffa3
rueblimaster <75255906+rueblimaster@users.noreply.github.com>
2025-09-02 17:25:18 +0200

    removed unused imports

--- src/main/java/at/hannibal2/skyhanni/events/GuiRenderEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/GuiRenderEvent.kt
@@ -1,7 +1,6 @@
 package at.hannibal2.skyhanni.events
 
 import at.hannibal2.skyhanni.api.event.RenderingSkyHanniEvent
-import at.hannibal2.skyhanni.events.render.gui.ScreenDrawnEvent
 import at.hannibal2.skyhanni.utils.compat.DrawContext
 
 open class GuiRenderEvent(context: DrawContext) : RenderingSkyHanniEvent(context) {
