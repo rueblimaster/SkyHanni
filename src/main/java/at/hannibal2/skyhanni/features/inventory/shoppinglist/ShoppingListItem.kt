c5205aa593381ffd5c177fbb98ba55ecf4966a18
rueblimaster <75255906+rueblimaster@users.noreply.github.com>
2025-09-03 12:32:04 +0200

    change amounts to Double and make display string good

--- src/main/java/at/hannibal2/skyhanni/features/inventory/shoppinglist/ShoppingListItem.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/shoppinglist/ShoppingListItem.kt
@@ -3,6 +3,7 @@ package at.hannibal2.skyhanni.features.inventory.shoppinglist
 import at.hannibal2.skyhanni.utils.ItemUtils.itemNameWithoutColor
 import at.hannibal2.skyhanni.utils.ItemUtils.repoItemName
 import at.hannibal2.skyhanni.utils.NeuInternalName
+import at.hannibal2.skyhanni.utils.NumberUtil.roundTo
 import at.hannibal2.skyhanni.utils.PrimitiveRecipe
 import at.hannibal2.skyhanni.utils.RecipeResolver
 import at.hannibal2.skyhanni.utils.renderables.Renderable
@@ -12,7 +13,7 @@ class ShoppingListItem(
     @Expose
     val internalName: NeuInternalName,
     @Expose
-    var amount: Int,
+    var amount: Double,
     recipe: PrimitiveRecipe? = null,
     @Expose
     val parentItem: ShoppingListItem? = null,
@@ -22,6 +23,9 @@ class ShoppingListItem(
 
     @Expose
     var subitems = listOf<ShoppingListItem>()
+
+    val totalAmount: Double
+        get() = if (parentItem == null) amount else amount * parentItem.totalAmount
 
     override fun toString(): String {
         return "${internalName.itemNameWithoutColor} x$amount: $recipeResolver"
@@ -34,19 +38,40 @@ class ShoppingListItem(
     private fun breakDown() {
         if (!recipeResolver.resolved) return
 
-        val ingredients: Map<NeuInternalName, Int> =
-            recipeResolver.recipe?.ingredients?.groupingBy { it.internalName }?.fold(0) { acc, ing -> acc + ing.count.toInt() } ?: return
+        val numOutputPerCraft = recipeResolver.recipe?.output?.count ?: return
 
-        subitems = ingredients.map { (internalName, amount) -> ShoppingListItem(internalName, amount, parentItem = this) }
+        val ingredients: Map<NeuInternalName, Double> =
+            recipeResolver.recipe?.ingredients?.groupingBy { it.internalName }?.fold(0.0) { acc, ing -> acc + ing.count } ?: return
+
+        subitems = ingredients.map { (internalName, amount) ->
+            ShoppingListItem(internalName, amount / numOutputPerCraft, parentItem = this)
+        }
 
         ShoppingList.update()
+    }
+
+    private fun Double.clean(): String =
+        if (this % 1.0 == 0.0) this.toInt().toString() else this.roundTo(2).toString()
+
+    private fun getDisplayString(indent: Int): String {
+        var displayString = "§8${"-".repeat(indent)}"
+
+        if (parentItem != null) {
+            displayString += "§7${amount.clean()}x "
+        }
+
+        displayString += "${internalName.repoItemName} "
+
+        displayString += "§fx${totalAmount.clean()}"
+
+        return displayString
     }
 
     fun buildDisplay(indent: Int = 0): List<Renderable> {
         return buildList {
             add(
                 Renderable.clickable(
-                    "§8${"-".repeat(indent)}${internalName.repoItemName}§f x$amount",
+                    getDisplayString(indent),
                     onLeftClick = ::triggerBreakDown,
                 ),
             )
