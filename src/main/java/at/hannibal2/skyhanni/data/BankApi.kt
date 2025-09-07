3d352fab9dcfae20ddd6416a47fa0d1ae898b175
rueblimaster <75255906+rueblimaster@users.noreply.github.com>
2025-09-07 15:21:27 +0200

    lay skeleton of BankApi

---
+++ src/main/java/at/hannibal2/skyhanni/data/BankApi.kt
@@ -0,0 +1,10 @@
+package at.hannibal2.skyhanni.data
+
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+
+@SkyHanniModule
+object BankApi {
+    // TODO: implement
+
+    val totalCoins: Double get() = 0.0
+}
